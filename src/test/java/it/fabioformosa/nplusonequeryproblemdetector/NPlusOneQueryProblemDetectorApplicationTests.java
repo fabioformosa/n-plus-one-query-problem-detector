1a501146f8b57a61022849cba097c652fd1083ff
fabio.formosa <fabio.formosa@gmail.com>
2025-07-22 19:47:47 +0200

    #1 first commit

---
+++ src/test/java/it/fabioformosa/nplusonequeryproblemdetector/NPlusOneQueryProblemDetectorApplicationTests.java
@@ -0,0 +1,14 @@
+package it.fabioformosa.nplusonequeryproblemdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NPlusOneQueryProblemDetectorApplicationTests {
+
+	@SuppressWarnings("java:S1186")
+	@Test
+	void contextLoads() {
+	}
+
+}
